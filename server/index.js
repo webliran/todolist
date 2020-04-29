f14b88cbd27dd54664263ea472f47dbe0f8c12c3
webliran <liranhecht@gmail.com>
2020-04-29 11:39:58 +0300

    third lesson

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const server = require('express')
 const app = server()
 const TodoRouter = require('./routes/todoRoute')
+const cors = require('cors');
 const port = 5000
 
+app.use(cors())
 
 app.use(server.json())
 app.use(server.urlencoded())
 
 app.use("/todo",TodoRouter)
+
 
 app.listen(port, function(){ console.log("listen to port " + port)})
